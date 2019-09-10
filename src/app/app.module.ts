81c20de2dbabae1044d4c2ccd27c3aee982d21b1
liendson.silva <liendson.silva@receita.pb.gov.br>
2019-09-10 11:10:32 -0300

    Adicionando Layout Padrão da Aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
+// Imports do Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Imports de Dependencias
+
+// Imports de Módulos/Componentes/Services
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
 import { FooterComponent } from './template/footer/footer.component';
+import { LoginLogoutComponent } from './template/login-logout/login-logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginLogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public title = 'Redetex Ambientações';
+  public usuario = 'Liendson';
+
   constructor() { }
 
   ngOnInit() {
