56891505bf2384e9627f330774e67b8087dc79f5
liendson.silva <liendson.silva@receita.pb.gov.br>
2019-09-13 15:05:15 -0300

    Adicionando Dependências

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginLogoutComponent } from './template/login-logout/login-logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginLogoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
